90ad8fb016808a7d45b5cb1a4920518d9ba92c23
ncardozo <n.cardozo@uniandes.edu.co>
2023-06-06 11:15:28 +0200

    adding config files
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

---
+++ metaprograming/nodes/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    val nodes : MutableList<Node>
+    nodes = mutableListOf<Node>()
+    nodes.add(Node(0, emptyList<Node>()))
+    for(i in 1..6) {
+        val n = when((1..3).random()) {
+            1 -> Computer(i, listOf(nodes[i-1]))
+            2 -> Printer(i, listOf(nodes[i-1]))
+            else -> Node(i, listOf(nodes[i-1]))
+        }
+        nodes.add(n)
+    }
+
+    val info = Information(3, "data to process as a string")
+    val info2 = Information(6, "Text for the printer")
+    nodes.last().handle(info)
+    nodes.last().handle(info2)
+}
